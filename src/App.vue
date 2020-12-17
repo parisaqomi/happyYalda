9a00b79b78376a4842004935d4b91965bd61df3b
parisaqomi <parisaq97@gmail.com>
2020-12-17 20:00:49 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="container d-flex justify-content-center align-items-center h-90">
+      <RandomOmen />
+    </div>
+    <div class="footer-image">
+ 
+    </div>
+  </div>
+</template>
+
+<script>
+import RandomOmen from "./components/RandomOmen.vue";
+
+export default {
+  name: "App",
+  components: {
+    RandomOmen
+  }
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "iransans";
+  src: url(assets/fonts/ttf/IRANSansWeb.ttf),
+    url(assets/fonts/eot/IRANSansWeb.eot),
+    url(assets/fonts/woff/IRANSansWeb.woff),
+    url(assets/fonts/woff2/IRANSansWeb.woff2);
+}
+
+body {
+  font-family: "iransans" !important;
+  box-sizing: border-box !important;
+  margin: 0 !important;
+  font-size: 14px !important;
+  direction: rtl !important;
+  text-align: right !important;
+}
+.h-90 {
+  height: 90vh;
+}
+</style>
---
+++ src/components/RandomOmen.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row text-center">
+    <div class="col-12">
+      <h3 class="my-3">یلداتون مبارک</h3>
+      <p class="my-3">برای گرفتن فال امشبتون فقط کافیه دکمه زیر رو فشار بدی</p>
+      <b-button class="my-3" @click="randomFaal" variant="danger" v-b-modal="'my-modal'">نمایش فال</b-button>
+    </div>
+    <div class="footer-image w-100"></div>
+    <div class="col-12">
+      <b-modal id="my-modal" scrollable centered>
+        <p>{{randomOmen.omen}}</p>
+        <p>{{randomOmen.meaning}}</p>
+        <template #modal-footer>
+          <div class="w-100 d-none"> 
+          </div>
+        </template>
+      </b-modal>
+    </div>
+  </div>
+</template>
+<style>
+.footer-image {
+   
+}
+
+.footer-image:after {
+    content: url(/pubilc/img/1x/yalda-bg.png);
+    top: 0;
+}
+</style>
+<script>
+import omensData from "../../faal.json";
+
+export default {
+  name: "RandomOmen",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      omens: omensData,
+      randomOmen: ""
+    };
+  },
+  methods: {
+    randomFaal: function() {
+      const idx = Math.floor(Math.random() * this.omens.length);
+      this.randomOmen = this.omens[idx];
+      return this.randomOmen;
+    }
+  }
+};
+</script>
+ 
