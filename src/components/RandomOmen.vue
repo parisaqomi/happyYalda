883f306377adfb049da1dae970ae8351de895752
parisaqomi <parisaq97@gmail.com>
2020-12-18 21:07:23 +0330

    update text

--- src/components/RandomOmen.vue
+++ src/components/RandomOmen.vue
@@ -2,26 +2,28 @@
   <div class="row text-center">
     <div class="col-12">
       <h3 class="my-3">یلداتون مبارک</h3>
-      <p class="my-3">برای گرفتن فال امشبتون فقط کافیه دکمه زیر رو فشار بدی</p>
+      <p
+        class="my-3"
+      >یکی از آئین های ایرانیان در شب یلدا خواندن شعرهای حافظ و تفال بر دیوان لسان الغیب است.
+      نیت کنید و فال حافظتون رو ببینید
+      </p>
       <b-button class="my-3" @click="randomFaal" variant="danger" v-b-modal="'my-modal'">نمایش فال</b-button>
     </div>
     <div class="col-12">
       <b-modal id="my-modal" scrollable centered>
-        <b-card bg-variant="danger" text-variant="white" title="فال شما">
-  <b-card-text>
-    <p v-html="randomOmen.omen"></p>
-  </b-card-text>
-</b-card>
-        <b-card border-variant="danger" text-variant="dark" title="تفسیر فال شما">
-  <b-card-text>
-        <p>{{randomOmen.meaning}}</p>
+        <b-card bg-variant="danger" text-variant="white" title="فال شما" class="mb-2">
+          <b-card-text>
+            <p v-html="randomOmen.omen"></p>
+          </b-card-text>
+        </b-card>
+        <b-card border-variant="danger" text-variant="dark" title="تفسیر  ">
+          <b-card-text>
+            <p>{{randomOmen.meaning}}</p>
+          </b-card-text>
+        </b-card>
 
-  </b-card-text>
-</b-card>
-        
         <template #modal-footer>
-          <div class="w-100 d-none"> 
-          </div>
+          <div class="w-100 d-none"></div>
         </template>
       </b-modal>
     </div>
@@ -29,7 +31,7 @@
 </template>
 <script>
 import omensData from "../../faal.json";
-import generateRandomIntegerWithin from "../helpers/randomGenerator"
+import generateRandomIntegerWithin from "../helpers/randomGenerator";
 
 export default {
   name: "RandomOmen",
@@ -46,7 +48,7 @@ export default {
     randomFaal: function() {
       let upperBound = this.omens.length;
       let lowerBound = 0;
-      let randomIndex = generateRandomIntegerWithin(lowerBound,upperBound);
+      let randomIndex = generateRandomIntegerWithin(lowerBound, upperBound);
       let randomOmen = this.omens[randomIndex];
       this.randomOmen = randomOmen;
       return;
